c8d2dbe440b3ef76bddad3976edd04561b1808ed
Mark Reijn <mark.reijn@hva.nl>
2024-11-13 16:14:56 +0100

    Initial Springboot application

---
+++ src/test/java/movie_master/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package movie_master;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
