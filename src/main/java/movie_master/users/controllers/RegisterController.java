efa9cb96649923ad42f2b44c03116bf9d397d59f
Ervin Bošković <ervin.boskovic@hva.nl>
2024-11-14 13:59:01 +0100

    feat(register user): added register controller and security configuration

--- src/main/java/movie_master/users/controllers/RegisterController.java
+++ src/main/java/movie_master/users/controllers/RegisterController.java
@@ -1,2 +1,15 @@
-package movie_master.users.controllers;public class RegisterController {
+package movie_master.users.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/register")
+public class RegisterController {
+
+    @PostMapping
+    private String register() {
+        return "User Registered Successfully";
+    }
 }
