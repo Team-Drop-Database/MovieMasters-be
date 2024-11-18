591eda8aafcffa0c9c0923254836d24054e936cf
Mark Reijn <mark.reijn@hva.nl>
2024-11-18 17:59:42 +0100

    Added env variable for the api key and check if there are movies in the db

--- src/main/java/movie_master/DataLoader.java
+++ src/main/java/movie_master/DataLoader.java
@@ -8,13 +8,19 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class DataLoader implements ApplicationRunner {
     private MovieRepository movieRepository;
+
+    @Value("${mdb.api-key}")
+    private String apiKey;
 
     @Autowired
     public DataLoader(MovieRepository movieRepository) {
@@ -22,20 +28,27 @@ public class DataLoader implements ApplicationRunner {
     }
 
     public void run(ApplicationArguments args) {
-        //TODO only do request if table is empty
+        // Only adding movies if there are non in the database
+        List<Movie> movies = movieRepository.findAll();
+        if (!movies.isEmpty()) {
+            return;
+        }
+
         OkHttpClient client = new OkHttpClient();
         ObjectMapper objectMapper = new ObjectMapper();
 
+        // Collecting movies from the movie database api
         Request request = new Request.Builder()
                 .url("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1")
                 .get()
                 .addHeader("accept", "application/json")
-                .addHeader("Authorization", "Bearer ")
+                .addHeader("Authorization", "Bearer %s".formatted(apiKey))
                 .build();
 
         try (Response response = client.newCall(request).execute()) {
             assert response.body() != null;
 
+            // Iterating over the movies and adding them to the database
             JsonNode arrayNode = objectMapper.readTree(response.body().string()).get("results");
             for (JsonNode node : arrayNode) {
                 Movie movie = objectMapper.treeToValue(node, Movie.class);
