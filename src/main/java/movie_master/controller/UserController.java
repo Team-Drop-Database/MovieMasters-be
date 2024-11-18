ad858add62a4bf86e8be826f08feaf94c1df5d79
Thomas Linssen <t.linssen@live.nl>
2024-11-18 14:18:42 +0100

    Added user controller and an endpoint for the watchlist

---
+++ src/main/java/movie_master/controller/UserController.java
@@ -0,0 +1,26 @@
+package movie_master.controller;
+
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import movie_master.model.Movie;
+import movie_master.service.UserService;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/watchlist")
+    public Set<Movie> getWatchList(@RequestParam(required = true) int userId){
+        return userService.getWatchList(userId);
+    }
+
+}
