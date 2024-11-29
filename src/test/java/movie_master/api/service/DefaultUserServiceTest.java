e3e87cc704ace55682214694e1faff8c42d87377
Mark Reijn <mark.reijn.reijn@hva.nl>
2024-11-29 11:56:40 +0100

    Fix failing movie tests

--- src/test/java/movie_master/api/service/DefaultUserServiceTest.java
+++ src/test/java/movie_master/api/service/DefaultUserServiceTest.java
@@ -108,9 +108,9 @@ public class DefaultUserServiceTest {
         // Given
         long userId = 69;
         User user = new User("example@test.mail", "User McNameface", "password1234", "QA", true);
-        Movie movie1 = new Movie(1, "Pulp Fiction", "Fun adventures", Date.from(Instant.now()), "en-US", "there");
-        Movie movie2 = new Movie(2, "Lock Stock & Two Smoking Barrels", "Fun adventures", Date.from(Instant.now()), "en-EN", "there");
-        Movie movie3 = new Movie(3, "Se7en", "Fun adventures", Date.from(Instant.now()), "en-US", "there");
+        Movie movie1 = new Movie(1, "Pulp Fiction", "Fun adventures", Date.from(Instant.now()), "en-US", "there", 9);
+        Movie movie2 = new Movie(2, "Lock Stock & Two Smoking Barrels", "Fun adventures", Date.from(Instant.now()), "en-EN", "there", 9);
+        Movie movie3 = new Movie(3, "Se7en", "Fun adventures", Date.from(Instant.now()), "en-US", "there", 9);
         Set<UserMovie> expectedResult = Set.of(
             new UserMovie(user, movie1, false),
             new UserMovie(user, movie2, false),
