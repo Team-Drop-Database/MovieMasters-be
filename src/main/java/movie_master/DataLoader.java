83fb301fd6bdc2e1ce1caf599463c0f571db9032
Thomas Linssen <t.linssen@live.nl>
2024-12-02 22:09:35 +0100

    Added automatic adding of UserMovies in DataLoader

--- src/main/java/movie_master/DataLoader.java
+++ src/main/java/movie_master/DataLoader.java
@@ -4,14 +4,16 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import movie_master.api.model.Movie;
+import movie_master.api.model.User;
+import movie_master.api.model.UserMovie;
 import movie_master.api.repository.MovieRepository;
+import movie_master.api.repository.UserRepository;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,16 +24,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class DataLoader implements ApplicationRunner {
     private MovieRepository movieRepository;
+    private UserRepository userRepository;
 
     @Value("${tmdb.api-key}")
     private String apiKey;
 
     @Autowired
-    public DataLoader(MovieRepository movieRepository) {
+    public DataLoader(MovieRepository movieRepository, 
+                        UserRepository userRepository) {
         this.movieRepository = movieRepository;
+        this.userRepository = userRepository;
     }
 
     public void run(ApplicationArguments args) {
+
+        // ADDING MOVIES //
+
         // Only adding movies if there are non in the database
         List<Movie> movies = movieRepository.findAll();
         if (!movies.isEmpty()) {
@@ -57,12 +65,37 @@ public class DataLoader implements ApplicationRunner {
             for (JsonNode node : arrayNode) {
                 Movie movie = objectMapper.treeToValue(node, Movie.class);
                 movie.setPosterPath("https://image.tmdb.org/t/p/original%s".formatted(movie.getPosterPath()));
-                movie.setTmdbRating(
-                        BigDecimal.valueOf(movie.getTmdbRating()).setScale(1, RoundingMode.UP).doubleValue());
                 movieRepository.save(movie);
             }
         } catch (Exception e){
             throw new RuntimeException(e);
         }
+
+        // ADDING A USER WITH USERMOVIES //
+
+        // Check if there's a user
+        List<User> users = userRepository.findAll();
+        if (!users.isEmpty()) {
+            return;
+        }
+
+        // If not, create a new one
+        User user = new User("test.user@gmail.com", "Bobs", 
+        "Burgers", "user", true);
+        userRepository.save(user);
+
+        // Take some movies
+        List<Movie> sampleMovies = movieRepository.findAll().stream()
+        .limit(7).collect(Collectors.toList());
+
+        // Assign them to the user, set some on 'watched' others on 'unwatched'
+        sampleMovies.forEach((movie) -> {
+            UserMovie userMovie = new UserMovie(user, movie, Math.random() > 0.5);
+            userMovie.setMovie(movie);
+            user.addMovieToWatchlist(userMovie);
+        });
+
+        // Save
+        userRepository.save(user);
     }
 }
