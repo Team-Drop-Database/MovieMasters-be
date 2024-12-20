b702acbcf026317fabb6bce3aae82d378aec3d8b
Mark Reijn <mark.reijn@hva.nl>
2024-12-20 20:18:40 +0100

    Working on pagination for search results

--- src/main/java/movie_master/api/repository/MovieRepository.java
+++ src/main/java/movie_master/api/repository/MovieRepository.java
@@ -1,5 +1,6 @@
 package movie_master.api.repository;
 
+import org.springframework.data.domain.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,5 @@ import java.util.List;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
-    List<Movie> findMovieByTitleContaining(String title);
+    List<Movie> findMovieByTitleContaining(String title, Limit limit);
 }
--- src/main/java/movie_master/api/service/DefaultMovieService.java
+++ src/main/java/movie_master/api/service/DefaultMovieService.java
@@ -2,13 +2,17 @@ package movie_master.api.service;
 
 import movie_master.api.model.Movie;
 import movie_master.api.repository.MovieRepository;
+import org.springframework.data.domain.Limit;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class DefaultMovieService implements MovieService {
+
+    private final int NUMBER_OF_MOVIES_PER_PAGE = 20;
     private final MovieRepository movieRepository;
 
     public DefaultMovieService(MovieRepository movieRepository) {
@@ -20,7 +24,7 @@ public class DefaultMovieService implements MovieService {
     }
 
     public List<Movie> findByTitleContaining(String title) {
-        return movieRepository.findMovieByTitleContaining(title);
+        return movieRepository.findMovieByTitleContaining(title, Limit.of(NUMBER_OF_MOVIES_PER_PAGE));
     }
 
     public Optional<Movie> findById(Long id) {
