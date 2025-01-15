dae069b1e94a8ddfa3e7da8a95906e1449d21bb0
Mitchell-Tol <mitchelltol02@gmail.com>
2025-01-15 19:30:06 +0100

    feature: retrieve reports

--- src/main/java/movie_master/api/controller/ReportController.java
+++ src/main/java/movie_master/api/controller/ReportController.java
@@ -3,6 +3,7 @@ package movie_master.api.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import movie_master.api.controller.model.GenericResponse;
 import movie_master.api.dto.ReportDto;
+import movie_master.api.exception.ReportNotFoundException;
 import movie_master.api.exception.UserNotFoundException;
 import movie_master.api.request.CreateReportRequest;
 import movie_master.api.service.ReportService;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("reports")
@@ -29,6 +31,24 @@ public class ReportController {
             ReportDto createdReport = reportService.createReport(request);
             return ResponseEntity.created(URI.create(httpServletRequest.getRequestURI())).body(createdReport);
         } catch (UserNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(new GenericResponse(e.getMessage()));
+        }
+    }
+
+    @GetMapping("all")
+    public ResponseEntity<Object> retrieveAllReport() {
+        List<ReportDto> reports = reportService.getAllReports();
+        return ResponseEntity.ok(reports);
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> retrieveReport(@RequestParam long reportId) {
+        try {
+            ReportDto report = reportService.getReportById(reportId);
+            return ResponseEntity.ok(report);
+        } catch (ReportNotFoundException e) {
             return ResponseEntity.notFound().build();
         } catch (Exception e) {
             return ResponseEntity.internalServerError().body(new GenericResponse(e.getMessage()));
--- src/main/java/movie_master/api/service/ReportService.java
+++ src/main/java/movie_master/api/service/ReportService.java
@@ -1,14 +1,17 @@
 package movie_master.api.service;
 
 import movie_master.api.dto.ReportDto;
+import movie_master.api.exception.ReportNotFoundException;
 import movie_master.api.exception.UserNotFoundException;
-import movie_master.api.mapper.UserDtoMapper;
+import movie_master.api.mapper.ReportDtoMapper;
 import movie_master.api.model.Report;
 import movie_master.api.model.User;
 import movie_master.api.repository.ReportRepository;
 import movie_master.api.repository.UserRepository;
 import movie_master.api.request.CreateReportRequest;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,14 +19,14 @@ public class ReportService {
 
     private final ReportRepository reportRepository;
     private final UserRepository userRepository;
-    private final UserDtoMapper userDtoMapper;
+    private final ReportDtoMapper reportDtoMapper;
 
     public ReportService(
-        ReportRepository reportRepository, UserRepository userRepository, UserDtoMapper userDtoMapper
+        ReportRepository reportRepository, UserRepository userRepository, ReportDtoMapper reportDtoMapper
     ) {
         this.reportRepository = reportRepository;
         this.userRepository = userRepository;
-        this.userDtoMapper = userDtoMapper;
+        this.reportDtoMapper = reportDtoMapper;
     }
 
     public ReportDto createReport(CreateReportRequest request) throws UserNotFoundException {
@@ -34,10 +37,18 @@ public class ReportService {
         Report reportToCreate = new Report(reportedUser, request.reason());
         Report createdReport = reportRepository.save(reportToCreate);
 
-        return new ReportDto(
-            createdReport.getId(),
-            userDtoMapper.apply(createdReport.getReportedUser()),
-            createdReport.getReason()
-        );
+        return reportDtoMapper.mapToDto(createdReport);
+    }
+
+    public List<ReportDto> getAllReports() {
+        return reportRepository.findAll().stream().map(reportDtoMapper::mapToDto).toList();
+    }
+
+    public ReportDto getReportById(long reportId) throws ReportNotFoundException {
+        Optional<Report> foundReport = reportRepository.findById(reportId);
+        if (foundReport.isEmpty()) { throw new ReportNotFoundException(reportId); }
+        Report report = foundReport.get();
+
+        return reportDtoMapper.mapToDto(report);
     }
 }
