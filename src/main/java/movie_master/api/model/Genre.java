253f1bf9f4d4387b78010d9c66f3a03af4b75ac2
Thomas Linssen <t.linssen@live.nl>
2025-01-17 16:27:09 +0100

    Added Genre model

---
+++ src/main/java/movie_master/api/model/Genre.java
@@ -0,0 +1,38 @@
+package movie_master.api.model;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Genre {
+
+    @Id
+    private long id;
+
+    @Column
+    private String name;
+
+    @ManyToMany(mappedBy = "genres")
+    private Set<Movie> movies = new HashSet<>();
+
+    public Genre() {}
+
+    public Genre(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
